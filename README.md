67ab1c4a48a90ab6fb5d28d8ed2a75114e482f26
Sudeep Prasad <sudeep.rp@gmail.com>
2024-09-22 18:46:56 +0530

    commandable arduino

--- README.md
+++ README.md
@@ -4,11 +4,32 @@
 
 ![nano pinout](pillar/arduino-nano-pinout.png "Arduino Nano pinout")
 
+---
+
+Servo ref: https://components101.com/motors/mg996r-servo-motor-datasheet
+
+![MG996R servo pinout](pillar/MG996R-Servo-Motor-Pinout.png "Servo pinout")
+
+---
+
+Proximity sensor ref: https://circuitdigest.com/microcontroller-projects/interfacing-e18d80nk-ir-proximity-sensor-with-arduino
+
+![E18-D80NK proximity pinout](pillar/E18-D80NK-Sensor-Pinout.png "Proximity sensor pinout")
+
 ## from last time
 
 Pin 9, 10, 11 used for servo
+Only pins 2 & 3 are interruptible
 Perhaps the pins marked with ~ are for output as well
 
 ## Pins
 
-`D2` used for input from hit. Contact with GND causes interrupt
+`D2` input from hit. Contact with GND causes interrupt
+
+`D4` input from placement. Low when placed 
+
+## upcoming trial
+
+Write from python using [write](https://pyserial.readthedocs.io/en/latest/pyserial_api.html)
+
+Read in Arduino using [Serial.read](https://www.arduino.cc/reference/en/language/functions/communication/serial/read/)
--- pillar/pillar.ino
+++ pillar/pillar.ino
@@ -1,23 +1,82 @@
-const int hitPin = 2;
+#include <Servo.h>
+
+Servo kickservo;
+Servo fangservo;
+
+const int hitpin = 2;
+const int crackpin = 3;
+const int placepin = 4;
+const int kickpin = 10;
+const int fangpin = 9;
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("v0.1 2in");
-  pinMode(hitPin, INPUT_PULLUP);
+  Serial.println("v0.9 hitint crack place fang kick");
+
+  pinMode(kickpin, OUTPUT);
+  kickservo.attach(kickpin);
+  kickservo.write(45);
+  pinMode(fangpin, OUTPUT);
+  fangservo.attach(fangpin);
+  fangservo.write(50);
+
+  pinMode(crackpin, OUTPUT);
+  digitalWrite(crackpin, HIGH);
+  pinMode(hitpin, INPUT_PULLUP);
+  pinMode(placepin, INPUT);
   // interrupt source: https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
-  attachInterrupt(digitalPinToInterrupt(hitPin), pillarHit, FALLING);
+  attachInterrupt(digitalPinToInterrupt(hitpin), pillarHit, FALLING);
+  // only pins 2 & 3 are interruptible, so not interrupting on placement
+}
+
+void printJson(const char* key, const char* value) {
+  Serial.print("\"");
+  Serial.print(key);
+  Serial.print("\":\"");
+  Serial.print(value);
+  Serial.print("\"");
+}
+
+char* digitalPinAsJson(int pin, const char* key) {
+  int value = digitalRead(pin);
+  if (value == HIGH) {
+    printJson(key, "H");
+  } else {
+    printJson(key, "L");
+  }
 }
 
 void loop() {
-  int hitValue = digitalRead(hitPin);
-  if (hitValue == HIGH) {
-    Serial.println('H');
-  } else {
-    Serial.println('L');
+  if (Serial.available() > 0) {
+    int incoming = Serial.read();
+    if (incoming == 'K') {
+      Serial.println("Kickout");
+      kickservo.write(0);
+    } else if (incoming == 'F') {
+      Serial.println("Fang");
+      fangservo.write(0);
+    } else if (incoming == 'C') {
+      Serial.println("Crack");
+      digitalWrite(crackpin, LOW);
+    } else if (incoming == 'R') {
+      Serial.println("Relax");
+      kickservo.write(45);
+      fangservo.write(50);
+      digitalWrite(crackpin, HIGH);
+    }
   }
-  delay(250);
+  Serial.print("{");
+  digitalPinAsJson(hitpin, "hit");
+  Serial.print(", ");
+  digitalPinAsJson(placepin, "placed");
+  Serial.println("}");
+  delay(500);
 }
 
 void pillarHit() {
-  Serial.println("CRACK");
+  Serial.println("{\"event\": \"CRACK\"}");
+}
+
+void hiranyaPlaced() {
+  Serial.println("{\"event\": \"HIRANYA\"}");
 }
